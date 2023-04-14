6ac446aad0a8ac4faae78aba98d7f1b3aadbe2b3
Fredrik <fresh@superburken.se>
2023-04-14 04:40:00 +0200

    formatting

--- src/func.c
+++ src/func.c
@@ -7,219 +7,219 @@
 
 struct range *get_ranges(unsigned long long n, int c) 
 {
-	unsigned long long size = floor(n / c);
-	unsigned long long remainder = n % c;
-	unsigned long long f, t;
-	int i;
+    unsigned long long size = floor(n / c);
+    unsigned long long remainder = n % c;
+    unsigned long long f, t;
+    int i;
 
-	struct range *ranges = malloc(c * sizeof(*ranges));
+    struct range *ranges = malloc(c * sizeof(*ranges));
 
-	if (ranges == NULL)
-		die("Error allocating memory");
+    if (ranges == NULL)
+        die("Error allocating memory");
 
-	f = 1;
-	t = size + remainder;
+    f = 1;
+    t = size + remainder;
 
-	for (i = 0; i < c; i++)
-	{
-		struct range item;
-		item.from = f;
-		item.to = t;
-		ranges[i] = item;
+    for (i = 0; i < c; i++)
+    {
+        struct range item;
+        item.from = f;
+        item.to = t;
+        ranges[i] = item;
 
-		f = t + 1;
-		t = f + size;
+        f = t + 1;
+        t = f + size;
 
-		if (t > n)
-			t = n;
-	}
+        if (t > n)
+            t = n;
+    }
 
-	return ranges;
+    return ranges;
 }
 
 void free_ranges(struct range *ranges) 
 {
-	free(ranges);
+    free(ranges);
 }
 
 void print_progress(unsigned long long n)
 {
-	char buf[PROGRESS_BAR_LENGTH + 1];
+    char buf[PROGRESS_BAR_LENGTH + 1];
 
-	int progress = ceil(100.0 * _total_count / n);
-	int progress_bar_count = ceil(progress / 100.0 * PROGRESS_BAR_LENGTH);
-	int i;
-	
-	printf("\r[");
-	
-	for (i = 0; i < PROGRESS_BAR_LENGTH; i++)
-	{
-		if ((i + 1) <= progress_bar_count)
-			buf[i] = '=';
-		else
-			buf[i] = ' ';
-	}
-	
-	buf[i] = '\0';
-	printf("%s", buf);
-	printf("] %d%% (%llu/%llu)", progress, _total_count, n);
+    int progress = ceil(100.0 * _total_count / n);
+    int progress_bar_count = ceil(progress / 100.0 * PROGRESS_BAR_LENGTH);
+    int i;
+    
+    printf("\r[");
+    
+    for (i = 0; i < PROGRESS_BAR_LENGTH; i++)
+    {
+        if ((i + 1) <= progress_bar_count)
+            buf[i] = '=';
+        else
+            buf[i] = ' ';
+    }
+    
+    buf[i] = '\0';
+    printf("%s", buf);
+    printf("] %d%% (%llu/%llu)", progress, _total_count, n);
 
-	fflush(stdout);
+    fflush(stdout);
 }
 
 void print_progress_until_complete(unsigned long long n)
 {
-	while (_total_count < n)
-	{
-		print_progress(n);
+    while (_total_count < n)
+    {
+        print_progress(n);
 #ifdef _WIN32
-		Sleep(10);
+        Sleep(10);
 #else
-		usleep(10000);
+        usleep(10000);
 #endif
-	}
+    }
 }
 
 int get_processor_count()
 {
-	int count = 1;
+    int count = 1;
 
 #ifdef _WIN32
-	LPSYSTEM_INFO info = malloc(sizeof(SYSTEM_INFO));
+    LPSYSTEM_INFO info = malloc(sizeof(SYSTEM_INFO));
 
-	if (info == NULL)
-		die("Error allocating memory");
-	
-	GetNativeSystemInfo(info);
-	count = (int)info->dwNumberOfProcessors;
-	free(info);
+    if (info == NULL)
+        die("Error allocating memory");
+    
+    GetNativeSystemInfo(info);
+    count = (int)info->dwNumberOfProcessors;
+    free(info);
 #else
-	count = (int)sysconf(_SC_NPROCESSORS_ONLN);
+    count = (int)sysconf(_SC_NPROCESSORS_ONLN);
 #endif
-	if (count < 1)
-		count = 1;
+    if (count < 1)
+        count = 1;
 
-	return count;
+    return count;
 }
 
 int is_prime(unsigned long long number)
 {
-	if (number < 2) 
-		return 0;
+    if (number < 2) 
+        return 0;
 
-	if (number == 2 || number == 3)
-		return 1;
+    if (number == 2 || number == 3)
+        return 1;
 
-	if (number % 2 == 0 || number % 3 == 0)
-		return 0;
-	
-	unsigned long long j, end = ceil(sqrt(number));
+    if (number % 2 == 0 || number % 3 == 0)
+        return 0;
+    
+    unsigned long long j, end = ceil(sqrt(number));
 
-	for (j = 5; j <= end; j++) {
-		if (number % j == 0)
-			return 0;
-	}
+    for (j = 5; j <= end; j++) {
+        if (number % j == 0)
+            return 0;
+    }
 
-	return 1;
+    return 1;
 }
 
 void increment_prime_count(unsigned long long n)
 {
 #ifdef _WIN32
-	InterlockedExchangeAdd64(&_prime_count, n);
+    InterlockedExchangeAdd64(&_prime_count, n);
 #else
-	pthread_mutex_lock(&_prime_count_lock);
-	_prime_count += n;
-	pthread_mutex_unlock(&_prime_count_lock);
+    pthread_mutex_lock(&_prime_count_lock);
+    _prime_count += n;
+    pthread_mutex_unlock(&_prime_count_lock);
 #endif
 }
 
 void increment_total_count(unsigned long long n)
 {
 #ifdef _WIN32
-	InterlockedExchangeAdd64(&_total_count, n);
+    InterlockedExchangeAdd64(&_total_count, n);
 #else
-	pthread_mutex_lock(&_total_count_lock);
-	_total_count += n;
-	pthread_mutex_unlock(&_total_count_lock);
+    pthread_mutex_lock(&_total_count_lock);
+    _total_count += n;
+    pthread_mutex_unlock(&_total_count_lock);
 #endif
 }
 
 void crunch_range(unsigned long long from, unsigned long long to)
 {
-	unsigned long long i;
-	unsigned long long prime_count = 0;
-	unsigned long long total_count = 0;
+    unsigned long long i;
+    unsigned long long prime_count = 0;
+    unsigned long long total_count = 0;
 
-	for (i = from; i <= to; i++)
-	{
-		if (is_prime(i))
-			prime_count++;
+    for (i = from; i <= to; i++)
+    {
+        if (is_prime(i))
+            prime_count++;
 
-		total_count++;
+        total_count++;
 
-		if (i % 100000 == 0) {
-			increment_prime_count(prime_count);
-			increment_total_count(total_count);
-			prime_count = 0;
-			total_count = 0;
-		}
-	}
+        if (i % 100000 == 0) {
+            increment_prime_count(prime_count);
+            increment_total_count(total_count);
+            prime_count = 0;
+            total_count = 0;
+        }
+    }
 
-	increment_prime_count(prime_count);
-	increment_total_count(total_count);
+    increment_prime_count(prime_count);
+    increment_total_count(total_count);
 }
 
 #ifdef _WIN32
 unsigned __stdcall crunch_range_on_thread(void *args)
 {
-	struct range *thread_range = (struct range *)args;
-	crunch_range(thread_range->from, thread_range->to);
-	_endthreadex(0);
-	return 0;
+    struct range *thread_range = (struct range *)args;
+    crunch_range(thread_range->from, thread_range->to);
+    _endthreadex(0);
+    return 0;
 }
 #else
 void *crunch_range_on_thread(void *args)
 {
-	struct range *thread_range = (struct range *)args;
-	crunch_range(thread_range->from, thread_range->to);
-	pthread_exit(0);
-	return NULL;
+    struct range *thread_range = (struct range *)args;
+    crunch_range(thread_range->from, thread_range->to);
+    pthread_exit(0);
+    return NULL;
 }
 #endif
 
 void parse_args(int argc, char** argv, unsigned long long *n, int *t)
 {
-	int i;
+    int i;
 
-	for (i = 1; i < argc; i++)
-	{
-		if (argv[i][0] != '-')
-			continue;
-		
-		if (argc > i + 1)
-		{
-			switch(argv[i][1])
-			{
-				case 't': 
-					*t = atol(argv[i+1]);
-					break;
-				case 'n':
-					*n = atol(argv[i+1]);
-					break;
-				default:
-					break;
-			}
-		}
-		else 
-		{
-			die("\nInvalid arguments! -t [number of threads] -n [number to crunch]\n");
-		}
-	}
+    for (i = 1; i < argc; i++)
+    {
+        if (argv[i][0] != '-')
+            continue;
+        
+        if (argc > i + 1)
+        {
+            switch(argv[i][1])
+            {
+                case 't': 
+                    *t = atol(argv[i+1]);
+                    break;
+                case 'n':
+                    *n = atol(argv[i+1]);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else 
+        {
+            die("\nInvalid arguments! -t [number of threads] -n [number to crunch]\n");
+        }
+    }
 }
 
 void die(char *msg)
 {
-	printf("\n%s\n", msg);
-	exit(1);
+    printf("\n%s\n", msg);
+    exit(1);
 }
--- src/main.c
+++ src/main.c
@@ -15,102 +15,102 @@ pthread_mutex_t _total_count_lock;
 
 void print_header() 
 {
-	printf("\nsimple cpu benchmark\n");
-	printf("freshbit.se\n\n");
-	fflush(stdout);
+    printf("\nsimple cpu benchmark\n");
+    printf("freshbit.se\n\n");
+    fflush(stdout);
 }
 
 int main(int argc, char* argv[])
 {
-	print_header();
+    print_header();
 
-	unsigned long long big_number = DEFAULT_NUMBER;
-	int number_of_threads = get_processor_count();
+    unsigned long long big_number = DEFAULT_NUMBER;
+    int number_of_threads = get_processor_count();
 
-	if (argc > 1)
-		parse_args(argc, argv, &big_number, &number_of_threads);
+    if (argc > 1)
+        parse_args(argc, argv, &big_number, &number_of_threads);
 
-	if (number_of_threads > MAX_NUMBER_OF_THREADS)
-		number_of_threads = MAX_NUMBER_OF_THREADS;
-	
-	if (big_number <= 0)
-		big_number = DEFAULT_NUMBER;
+    if (number_of_threads > MAX_NUMBER_OF_THREADS)
+        number_of_threads = MAX_NUMBER_OF_THREADS;
+    
+    if (big_number <= 0)
+        big_number = DEFAULT_NUMBER;
 
-	if (big_number < 10000)
-		number_of_threads = 1;
-	
-	struct range *ranges = get_ranges(big_number, number_of_threads);
-	
+    if (big_number < 10000)
+        number_of_threads = 1;
+    
+    struct range *ranges = get_ranges(big_number, number_of_threads);
+    
 #ifdef _WIN32
-	HANDLE threads[MAX_NUMBER_OF_THREADS];
-	unsigned thread_ids[MAX_NUMBER_OF_THREADS];
+    HANDLE threads[MAX_NUMBER_OF_THREADS];
+    unsigned thread_ids[MAX_NUMBER_OF_THREADS];
 #else
-	pthread_t threads[MAX_NUMBER_OF_THREADS];
+    pthread_t threads[MAX_NUMBER_OF_THREADS];
 #endif
-	
+    
 
 #ifndef _WIN32
-	if (pthread_mutex_init(&_prime_count_lock, NULL) != 0)
-		die("pthread_mutex_init failed");
-	if (pthread_mutex_init(&_total_count_lock, NULL) != 0)
-		die("pthread_mutex_init failed");
+    if (pthread_mutex_init(&_prime_count_lock, NULL) != 0)
+        die("pthread_mutex_init failed");
+    if (pthread_mutex_init(&_total_count_lock, NULL) != 0)
+        die("pthread_mutex_init failed");
 #endif
 
-	printf("crunching primes from number %llu using %d threads\n", big_number, number_of_threads);
-	fflush(stdout);
+    printf("crunching primes from number %llu using %d threads\n", big_number, number_of_threads);
+    fflush(stdout);
 
-	int i;
-	time_t start_time, end_time, elapsed_time;
-	
-	time(&start_time);
+    int i;
+    time_t start_time, end_time, elapsed_time;
+    
+    time(&start_time);
 
-	for (i = 0; i < number_of_threads; i++)
-	{	
+    for (i = 0; i < number_of_threads; i++)
+    {	
 #ifdef DEBUG
-		printf("f: %llu t: %llu\n", ranges[i].from, ranges[i].to);
+        printf("f: %llu t: %llu\n", ranges[i].from, ranges[i].to);
 #endif
 
-		/* _beginthreadex returns 0 on error. pthread returns 0 on success :) */
+        /* _beginthreadex returns 0 on error. pthread returns 0 on success :) */
 #ifdef _WIN32
-		threads[i] = (HANDLE)_beginthreadex(NULL, 0, &crunch_range_on_thread, &ranges[i], 0, &thread_ids[i]);
-		
-		if (threads[i] == 0)
-			die("Error creating thread");
+        threads[i] = (HANDLE)_beginthreadex(NULL, 0, &crunch_range_on_thread, &ranges[i], 0, &thread_ids[i]);
+        
+        if (threads[i] == 0)
+            die("Error creating thread");
 #else	
-		if (pthread_create(&threads[i], NULL, &crunch_range_on_thread, &ranges[i]) != 0)
-			die("Error creating thread");
+        if (pthread_create(&threads[i], NULL, &crunch_range_on_thread, &ranges[i]) != 0)
+            die("Error creating thread");
 #endif
-	}
+    }
 
-	printf("\n");
-	print_progress_until_complete(big_number);
+    printf("\n");
+    print_progress_until_complete(big_number);
 
 #ifdef _WIN32
-	WaitForMultipleObjects(number_of_threads, threads, 1, INFINITE);
+    WaitForMultipleObjects(number_of_threads, threads, 1, INFINITE);
 #endif
 
-	for (i = 0; i < number_of_threads; i++)
-	{
+    for (i = 0; i < number_of_threads; i++)
+    {
 #ifdef _WIN32
-		CloseHandle(threads[i]);
+        CloseHandle(threads[i]);
 #else
-		if (pthread_join(threads[i], NULL) != 0)
-			die("Error joining thread");
+        if (pthread_join(threads[i], NULL) != 0)
+            die("Error joining thread");
 #endif
-	}
+    }
 
-	time(&end_time);
-	elapsed_time = end_time - start_time;
-	
-	print_progress(big_number);
-	printf("\n\nFound %llu primes in %ld seconds\n\n", _prime_count, elapsed_time);
+    time(&end_time);
+    elapsed_time = end_time - start_time;
+    
+    print_progress(big_number);
+    printf("\n\nFound %llu primes in %ld seconds\n\n", _prime_count, elapsed_time);
 
 #ifndef _WIN32
-	pthread_mutex_destroy(&_prime_count_lock);
-	pthread_mutex_destroy(&_total_count_lock);
+    pthread_mutex_destroy(&_prime_count_lock);
+    pthread_mutex_destroy(&_total_count_lock);
 #endif
 
-	free_ranges(ranges);
+    free_ranges(ranges);
 
-	return 0;
+    return 0;
 }
